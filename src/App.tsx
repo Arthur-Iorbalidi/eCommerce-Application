22943e5a176b7fa94a8e60fbfcd1a9c1c0ef5f2f
Stanislav Shkradov <studingDev@gmail.com>
2024-05-12 12:30:14 +0300

    fix: fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './components/Main/main';
 import Error from './components/Error/Error';
+
+import Registration from './components/Registration/registration';
+import Login from './components/Login/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/*" element={<Main />} />
+        <Route path="/" element={<Main />}>
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
 
-        <Route path="*" element={<Navigate to="/error" />} />
-        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,6 +1,6 @@
 import './main.scss';
 
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Registration from '../Registration/registration';
 import Header from './components/header/header';
@@ -11,9 +11,8 @@ function Main() {
     <div className="main-page">
       <Header />
       <Routes>
-        <Route path="registration" element={<Registration />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*/*" element={<Navigate to="/error" />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
