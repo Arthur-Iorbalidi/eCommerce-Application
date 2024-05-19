b061d284dc25c023553a3573dfa4612fd4aab999
Violetta Stolarova <violettastolarova@icloud.com>
2024-05-19 13:52:10 +0300

    feat: add redux-toolkit

--- src/components/Main/components/header/components/sideMenu/sideMenu.tsx
+++ src/components/Main/components/header/components/sideMenu/sideMenu.tsx
@@ -1,32 +1,24 @@
 import './sideMenu.scss';
 
-import { useNavigate } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-
 import { BsXLg } from 'react-icons/bs';
-import { SideMenuReducerType } from '../../../../../../redux/sideMenuReducer';
+
+import { useNavigate } from 'react-router-dom';
+import useAppDispatch from '../../../../../../shared/hooks/useAppDispatch';
+import { activateSideMenu } from '../../../../../../store/reducers/sideMenuSlice';
 
 import NavBar from '../navBar/navBar';
 import Button from '../../../../../../shared/ui/Button/Button';
 
-interface Props {
-  activateSideMenu: (data: boolean) => { type: string; data: boolean };
-}
-type DispatchType = Dispatch<{ type: string; data: boolean }>;
-type MapDispatch = {
-  activateSideMenu: (data: boolean) => { type: string; data: boolean };
-};
-
-function SideMenu({ activateSideMenu }: Props) {
+export default function SideMenu() {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   return (
     <div className="main-page_side-menu">
       <BsXLg
         className="main-page_side-menu_close"
         onClick={() => {
-          activateSideMenu(false);
+          dispatch(activateSideMenu(false));
         }}
       />
       <div className="main-page_side-menu_wrapper">
@@ -37,7 +29,7 @@ function SideMenu({ activateSideMenu }: Props) {
             color="green"
             className="header-button"
             onClick={() => {
-              activateSideMenu(false);
+              dispatch(activateSideMenu(false));
               navigate('login');
             }}
           />
@@ -45,7 +37,7 @@ function SideMenu({ activateSideMenu }: Props) {
             value="Sign Up"
             color="green"
             onClick={() => {
-              activateSideMenu(false);
+              dispatch(activateSideMenu(false));
               navigate('registration');
             }}
           />
@@ -54,19 +46,3 @@ function SideMenu({ activateSideMenu }: Props) {
     </div>
   );
 }
-
-function mapStateToProps(state: SideMenuReducerType): SideMenuReducerType {
-  return state;
-}
-
-function mapDispatchToProps(dispatch: DispatchType): MapDispatch {
-  return {
-    activateSideMenu: (data: boolean) =>
-      dispatch({
-        type: 'activateSideMenu',
-        data,
-      }),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SideMenu);
--- src/redux/root.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import sideMenuReducer from './sideMenuReducer';
-
-const rootReducer = combineReducers({
-  sideMenuReducer,
-});
-
-export default rootReducer;
