d8c584ddda5fccd8bed14ca700654ebc8ce883d0
Violetta <119078429+ViolettaStolarova@users.noreply.github.com>
2024-05-21 20:42:13 +0300

    feat: add logout button (#22)

--- src/components/Main/components/header/components/sideMenu/sideMenu.tsx
+++ src/components/Main/components/header/components/sideMenu/sideMenu.tsx
@@ -5,6 +5,8 @@ import { BsXLg } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 import useAppDispatch from '../../../../../../shared/hooks/useAppDispatch';
 import { activateSideMenu } from '../../../../../../store/reducers/sideMenuSlice';
+import useAppSelector from '../../../../../../shared/hooks/useAppSelector';
+import { activateAuthorizationState } from '../../../../../../store/reducers/authorizationState';
 
 import NavBar from '../navBar/navBar';
 import Button from '../../../../../../shared/ui/Button/Button';
@@ -12,6 +14,10 @@ import Button from '../../../../../../shared/ui/Button/Button';
 export default function SideMenu() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+
+  const authorizationState = useAppSelector(
+    (state) => state.authorizationStateReducer.isAuthorized,
+  );
 
   return (
     <div className="main-page_side-menu">
@@ -24,23 +30,37 @@ export default function SideMenu() {
       <div className="main-page_side-menu_wrapper">
         <NavBar position="side" />
         <div className="main-page_header_wrapper-all_buttons side">
-          <Button
-            value="Sign In"
-            color="green"
-            className="header-button"
-            onClick={() => {
-              dispatch(activateSideMenu(false));
-              navigate('login');
-            }}
-          />
-          <Button
-            value="Sign Up"
-            color="green"
-            onClick={() => {
-              dispatch(activateSideMenu(false));
-              navigate('registration');
-            }}
-          />
+          {!authorizationState ? (
+            <>
+              <Button
+                value="Sign In"
+                color="green"
+                className="header-button"
+                onClick={() => {
+                  dispatch(activateSideMenu(false));
+                  navigate('login');
+                }}
+              />
+              <Button
+                value="Sign Up"
+                color="green"
+                onClick={() => {
+                  dispatch(activateSideMenu(false));
+                  navigate('registration');
+                }}
+              />
+            </>
+          ) : (
+            <Button
+              value="Logout"
+              color="green"
+              className="header-button"
+              onClick={() => {
+                dispatch(activateSideMenu(false));
+                dispatch(activateAuthorizationState(false));
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
