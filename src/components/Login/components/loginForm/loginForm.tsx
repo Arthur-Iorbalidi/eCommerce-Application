2f89bc752771497c715b02501e8f35e81aeda5f3
Violetta <119078429+ViolettaStolarova@users.noreply.github.com>
2024-05-26 17:52:36 +0300

    fix: fix forms (#26)

--- src/components/Login/components/loginForm/loginForm.tsx
+++ src/components/Login/components/loginForm/loginForm.tsx
@@ -1,55 +1,28 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+
 import { BsEnvelopeFill, BsPersonFillLock } from 'react-icons/bs';
-import { Link, useNavigate } from 'react-router-dom';
 import Alert from 'react-bootstrap/Alert';
+
 import { useState } from 'react';
 import useAppDispatch from '../../../../shared/hooks/useAppDispatch';
 import { activateAuthorizationState } from '../../../../store/reducers/authorizationState';
+
 import Button from '../../../../shared/ui/Button/Button';
 import Input from '../../../../shared/ui/Input/Input';
 import Loader from '../../../../shared/ui/Loader/loader';
 import styles from './loginForm.module.scss';
+
 // api
 import logInUser from '../../../../services/api/actions/logInUser';
+import getValidationSchema from './validationSchema';
 
 interface LoginFormFields {
   email: string;
   password: string;
 }
-
-const validationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .required('Email is required')
-    .matches(/^[^\s]*$/, "Email mustn't contain spaces")
-    .test('containsAtSymbol', 'Email must contain @ symbol', (value) => {
-      const atRegex = /@/;
-      return atRegex.test(value);
-    })
-    .test('domain', 'Email must contain a domain name', (value) => {
-      const domainRegex = /@[^\s@]+\.[^\s@]+$/;
-      return domainRegex.test(value);
-    })
-    .email('Email is invalid'),
-  password: yup
-    .string()
-    .required('Password is required')
-    .matches(/^[^\s]*$/, "Password mustn't contain spaces")
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/[0-9]/, 'Password must contain at least one digit')
-    .test(
-      'specialChar',
-      'Password must contain at least one special character',
-      (value) => {
-        const specialCharRegex = /[@#$%^&*-+]/;
-        return specialCharRegex.test(value);
-      },
-    )
-    .min(8, 'Password must be at least 8 characters'),
-});
 
 function LoginForm() {
   const navigate = useNavigate();
@@ -59,6 +32,8 @@ function LoginForm() {
     text: '',
   });
   const [isLoading, setIsLoading] = useState(false);
+
+  const validationSchema = getValidationSchema();
 
   const {
     register,
@@ -124,7 +99,7 @@ function LoginForm() {
 
         {modal.isShowed && (
           <Alert
-            className={styles.allert}
+            className={styles.alert}
             variant="danger"
             onClose={() => {
               setModal({ isShowed: false, text: '' });
