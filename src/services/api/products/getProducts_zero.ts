b2ef631d88ae8777738e64ef6fafd8519e84dc24
Stanislav Shkradov <studingDev@gmail.com>
2024-05-30 22:10:37 +0300

    feat: fixed routing, add user cabinet

--- src/services/api/products/getProducts_zero.ts
+++ src/services/api/products/getProducts_zero.ts
@@ -19,25 +19,3 @@ export default function getProductsZero(
       }
     });
 }
-
-import {
-  ClientResponse,
-  ProductPagedQueryResponse,
-} from '@commercetools/platform-sdk';
-import zeroClientApi from '../ZeroClient';
-import { projectKey } from '../index';
-
-export default function getProductsZero(
-  callBack: (value: ClientResponse<ProductPagedQueryResponse>) => void,
-): void {
-  zeroClientApi()
-    .withProjectKey({ projectKey })
-    .products()
-    .get()
-    .execute()
-    .then((res) => {
-      if (res.statusCode === 200) {
-        callBack(res);
-      }
-    });
-}
