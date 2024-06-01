5993011670dadeab6e8f877c2a4443f5cd4ccae1
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2024-06-01 11:41:15 +0300

    feat: add discription to products

--- src/shared/ui/Product/productItem.tsx
+++ src/shared/ui/Product/productItem.tsx
@@ -14,6 +14,17 @@ function ProductItem(product: Props) {
     return price / 10 ** fractionDigits;
   }
 
+  function addDiscription(text?: string) {
+    if (!text) {
+      return [];
+    }
+
+    return text.split('\n').map((elem, index) => {
+      const key = `${index}_${elem}`;
+      return <span key={key}>{elem}</span>;
+    });
+  }
+
   return (
     <div className={styles.productItem}>
       <div className={styles.photoWrapper}>
@@ -24,31 +35,40 @@ function ProductItem(product: Props) {
         />
       </div>
       <div className={styles.content}>
-        <div className={styles.prices}>
-          <span className={styles.currentPrice}>
-            $
-            {calculatePrice(
-              product.value.masterData.current.masterVariant.prices?.[0].value
-                .centAmount as number,
-              product.value.masterData.current.masterVariant.prices?.[0].value
-                .fractionDigits as number,
-            )}
-          </span>
-          <span className={styles.usuallPrice}>
-            $
-            {calculatePrice(
-              product.value.masterData.current.masterVariant.prices?.[0].value
-                .centAmount as number,
-              product.value.masterData.current.masterVariant.prices?.[0].value
-                .fractionDigits as number,
-            )}
-          </span>
+        <div className={styles.prices_link}>
+          <div className={styles.prices}>
+            <span className={styles.currentPrice}>
+              $
+              {Math.round(
+                calculatePrice(
+                  product.value.masterData.current.masterVariant.prices?.[0]
+                    .value.centAmount as number,
+                  product.value.masterData.current.masterVariant.prices?.[0]
+                    .value.fractionDigits as number,
+                ) * 0.9,
+              )}
+            </span>
+            <span className={styles.usuallPrice}>
+              $
+              {calculatePrice(
+                product.value.masterData.current.masterVariant.prices?.[0].value
+                  .centAmount as number,
+                product.value.masterData.current.masterVariant.prices?.[0].value
+                  .fractionDigits as number,
+              )}
+            </span>
+          </div>
+          <Button value={(<FaShoppingBasket />) as ReactNode} color="green" />
         </div>
-        <div className={styles.name_link}>
+        <div className={styles.nameBlock}>
           <span className={styles.name}>
             {product.value.masterData.current.name.en}
           </span>
-          <Button value={(<FaShoppingBasket />) as ReactNode} color="green" />
+        </div>
+        <div className={styles.descriptionBlock}>
+          <span className={styles.description}>
+            {addDiscription(product.value.masterData.current.description?.en)}
+          </span>
         </div>
       </div>
     </div>
