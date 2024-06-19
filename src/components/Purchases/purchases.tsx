515fb9d78b71425e83c3170e15dfd6a86d9fe01c
Stas <99683205+StasDevel@users.noreply.github.com>
2024-06-19 22:28:07 +0300

    Feat/add cart (#51)
    * feat: add cart and most actions in it
    
    * feat: add cart and most actions in it
    
    * fixed some bages
    
    * fixed some bages
    
    * fixed some bages

--- src/components/Purchases/purchases.tsx
+++ src/components/Purchases/purchases.tsx
@@ -1,20 +1,14 @@
 import { useEffect, useState } from 'react';
-import {
-  ClientResponse,
-  Cart,
-  ProductProjection,
-} from '@commercetools/platform-sdk';
+import { ClientResponse, Cart } from '@commercetools/platform-sdk';
 
 import { Link } from 'react-router-dom';
 import getMyCart from '../../services/api/cart/getMyCart';
 import ProductForm from './components/productForm';
 import styles from './purchases.module.scss';
-import Button from '../../shared/ui/Button/Button';
-import deleteItemFromCart from '../../services/api/cart/deleteItemFromCart';
 import getCalculatedPrice from '../../services/helpers/getCalculatedPrice';
 
 export default function Purchases() {
-  const [cart, changeCart] = useState({});
+  const [cart, changeCart] = useState<Cart | undefined>();
   const [stateWithProducts, changeStateWithProducts] = useState([]);
 
   function putProductsInState(value: ClientResponse) {
@@ -23,19 +17,10 @@ export default function Purchases() {
   }
   useEffect(() => getMyCart(putProductsInState), []);
 
-  function clearCart() {
-    stateWithProducts.map((elem) => deleteItemFromCart(elem.productId));
-    getMyCart(putProductsInState);
-  }
-  // const navigate = useNavigate();
-  // function nav(value: string) {
-  //   navigate(`/catalog/product/${value}`);
-  // }
   return (
     <div>
       {stateWithProducts.length !== 0 ? (
         <div>
-          <Button value="clear cart" color="red" onClick={() => clearCart()} />
           <div className={styles.purchasesWrapper}>
             {stateWithProducts.map((elem, index) =>
               ProductForm({
@@ -45,11 +30,11 @@ export default function Purchases() {
               }),
             )}
           </div>
-          <div>{`Total cost: ${cart.totalPrice ? getCalculatedPrice(+cart.totalPrice.centAmount, 2) : 0}`}</div>
+          <div>{`Total cost: ${cart!.totalPrice! ? getCalculatedPrice(+cart!.totalPrice.centAmount, 2) : 0}`}</div>
         </div>
       ) : (
         <div>
-          Ваша корзина пуста
+          No products...
           <Link to="/catalog">Catalog</Link>
         </div>
       )}
